573746848a9713ccde7bc4f941df89cda4b14842
HongyangLiu <16132238+tiger-tooth@users.noreply.github.com>
2018-12-21 16:20:36 +0800

    jpg and xml rename

--- rename.py
+++ rename.py
@@ -8,7 +8,7 @@ class BatchRename():
     '''
     def __init__(self):
         #图片路径
-        self.path = 'you/images/and/xmls/path'
+        self.path = ''you/images/and/xmls/path''
  
     def rename(self):
         filelist = os.listdir(self.path)
@@ -16,8 +16,9 @@ class BatchRename():
         #此处的排序可以使得图片和对应的xml文件被命名为同一个名字
         filelist.sort()
         total_num = len(filelist)
-        i = 1
-        val=1
+        i = 1071
+        val_jpg=0
+        val_xml=0
         n = 6
         for item in filelist:
             if item.endswith('.jpg'):
@@ -26,16 +27,18 @@ class BatchRename():
                 dst = os.path.join(os.path.abspath(self.path), str(0)*n + str(i) + '.jpg')
                 os.rename(src, dst)
                 time.sleep(0.01)
+                val_jpg=val_jpg+1
                 print('converting %s to %s ...' % (src, dst))
 
-            else:
+            elif item.endswith('.xml'):
                 src = os.path.join(os.path.abspath(self.path), item)
                 dst = os.path.join(os.path.abspath(self.path), str(0)*n + str(i) + '.xml')
                 os.rename(src, dst)
                 time.sleep(0.01)
+                val_xml=val_xml+1
                 print('converting %s to %s ...' % (src, dst))
                 i=i+1            
-        print('total %d to rename & converted %d jpgs' % (total_num, i))
+        print('total %d to rename & converted %d jpgs & %d xmls' % (total_num, val_jpg,val_xml))
  
 if __name__ == '__main__':
     demo = BatchRename()
